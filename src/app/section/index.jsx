43b538afa581ced918ccbab0845c418f080f1394
rayssa <rayssagoncalves.tds2.senai@gmail.com>
2025-03-25 11:47:50 -0300

    feat: add components

---
+++ src/app/section/index.jsx
@@ -0,0 +1,13 @@
+import { Children } from 'react';
+import styles from './section.module.css'
+
+const Section = ({titulo, children}) => {
+    return (
+        <section className={styles.newTrophies}>
+        <h2>{titulo}</h2>
+        <div className={styles.trophiesGrid}>{children}</div>
+         </section>
+    );
+};
+
+export default Section;
--- src/app/titulos/page.jsx
+++ src/app/titulos/page.jsx
@@ -3,122 +3,53 @@ import styles from "./titulos.module.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Card from "../components/cards";
+import Section from "../section";
 
 export default function Titulos() {
   return (
     <div className={styles.container}>
       <Header />
       <main className={styles.main}>
-        <section className={styles.featured}>
-          <h2>Conquistas Principais</h2>
-          <div className={styles.featuredTrophies}>
-            <Card ano={2012} 
-            capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} 
-            detalhes={"Corinthians 1 x 0 Chelsea"} 
-            textoAlt={"Troféu do Mundial Interclubes 2012"} 
-            titulo={"Mundial de Clubes da FIFA"} />
 
-            <Card ano={2012} 
-            capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} 
-            detalhes={"Corinthians 2 x 0 Boca Juniors"} 
-            textoAlt={"Troféu da Copa Libertadores da América 2012"} 
-            titulo={"Copa Libertadores da América"} />
 
-            <Card ano={2000} 
-            capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"}
-            detalhes={"Corinthians 0(4) x 0(3) Vasco"}
-            textoAlt={"Troféu da Copa Libertadores da América 2000"}
-            titulo={"Mundial de Clubes da FIFA"} />
-          </div>
-        </section>
+        <Section titulo={"Conquistas Principais"}>
+            <Card ano={2012}
+              capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"}
+              detalhes={"Corinthians 1 x 0 Chelsea"}
+              textoAlt={"Troféu do Mundial Interclubes 2012"}
+              titulo={"Mundial de Clubes da FIFA"} />
 
-        <section className={styles.newTrophies}>
-          <h2>Campeonatos Brasileiros</h2>
-          <div className={styles.trophiesGrid}>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Brasileiro 2017"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Brasileiro</h3>
-                <p className={styles.year}>2017</p>
-                <p className={styles.details}>7º título brasileiro</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Brasileiro 2015"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Brasileiro</h3>
-                <p className={styles.year}>2015</p>
-                <p className={styles.details}>6º título brasileiro</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Brasileiro 2011"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Brasileiro</h3>
-                <p className={styles.year}>2011</p>
-                <p className={styles.details}>5º título brasileiro</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-          </div>
-        </section>
+            <Card ano={2012}
+              capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"}
+              detalhes={"Corinthians 2 x 0 Boca Juniors"}
+              textoAlt={"Troféu da Copa Libertadores da América 2012"}
+              titulo={"Copa Libertadores da América"} />
 
-        <section className={styles.promo}>
-          <h2>Campeonatos Estaduais</h2>
-          <div className={styles.trophiesGrid}>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Paulista 2023"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Paulista</h3>
-                <p className={styles.year}>2023</p>
-                <p className={styles.details}>
-                  <span className={styles.recordTitle}>
-                    30º título estadual
-                  </span>
-                </p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Campeonato Paulista 2019"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Campeonato Paulista</h3>
-                <p className={styles.year}>2019</p>
-                <p className={styles.details}>
-                  <span className={styles.recordTitle}>
-                    29º título estadual
-                  </span>
-                </p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
+            <Card ano={2000}
+              capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"}
+              detalhes={"Corinthians 0(4) x 0(3) Vasco"}
+              textoAlt={"Troféu da Copa Libertadores da América 2000"}
+              titulo={"Mundial de Clubes da FIFA"} />
+        </Section>
+
+        <Section titulo={"Campeonato Brasileiro"}>
+          <Card ano={2017} capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} detalhes={"7º título brasileiro"} textoAlt={"Troféu do Campeonato Brasileiro 2017"} titulo={"Campeonato Brasileiro"} />
+
+          <Card ano={2015} capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} detalhes={"6º título brasileiro"} textoAlt={"Troféu do Campeonato Brasileiro 2015"} titulo={"Campeonato Brasileiro"} />
+
+          <Card ano={2011} capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} detalhes={"5º título brasileiro"} textoAlt={"Troféu do Campeonato Brasileiro 2011"} titulo={"Campeonato Brasileiro"} />
+
+        </Section>
+
+        <Section titulo={"Campeonato Estadual"}>
+          <Card ano={2023} capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} detalhes={"30º título estadual"} textoAlt={"Troféu do Campeonato Paulista 2023"} titulo={"Campeonato Paulista"} />
+
+          <Card ano={2019} capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"} detalhes={"29º título estadual"} textoAlt={"Troféu do Campeonato Paulista 2019"} titulo={"Campeonato Paulista"} />
+
+          <Card ano={}
+          
+        </Section>
+      
             <div className={styles.trophy}>
               <div className={styles.trophyCover}>
                 <img
